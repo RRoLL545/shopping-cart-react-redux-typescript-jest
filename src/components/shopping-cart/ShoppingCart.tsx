c55250a0b634225e1be7ea28817bcfc32ac19888
Быков Роман <bykov@softlab.ru>
2022-03-17 18:22:38 +0300

    feat(footer) create Footer component
    Create Footer component and implement in ShoppingCart

--- src/components/shopping-cart/ShoppingCart.tsx
+++ src/components/shopping-cart/ShoppingCart.tsx
@@ -1,3 +1,4 @@
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import './shopping-cart.css';
 
@@ -9,14 +10,14 @@ const ShoppingCart = (): JSX.Element => {
   return (
     <>
       <div className="shopping-cart">
-        <Header></Header>
+        <Header />
         <main className="main">
           <div>ToggleAddItemFormButton</div>
           <div>AddItemForm</div>
           <div>Cart Item list</div>
         </main>
         <aside>Aside</aside>
-        <footer>Footer</footer>
+        <Footer />
       </div>
       <div>Checkoutdata</div>
       <div className="proceed-info invisible">You were redirected to checkout page...</div>
