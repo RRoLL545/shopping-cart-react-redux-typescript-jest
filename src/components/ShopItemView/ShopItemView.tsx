6b1c95ff73ab4df8d4fc85384daf7cddae843755
Быков Роман <bykov@softlab.ru>
2022-03-23 17:26:00 +0300

    feat(AddItemFromShop): create add Shop Item to cart feature
    Create add Shop Item to cart feature

--- src/components/ShopItemView/ShopItemView.tsx
+++ src/components/ShopItemView/ShopItemView.tsx
@@ -1,4 +1,5 @@
 import { ItemData } from '../../interfaces/itemData';
+import { addShopItemToCart } from '../../stateManagement/actionCreators/cartItems';
 import './ShopItemView.css';
 
 /**
@@ -14,9 +15,32 @@ const ShopItemView = (props: ItemData): JSX.Element => {
       <img src={ imageUrl } alt={ name } className={`shop-item-img shop-item-img-${id}`}></img>
       <h4 className="shop-item-name">{name}</h4>
       <div className={`item-price-${id}`}>{price} rub.</div>
-      <button className={`controls-button shop-item-add shop-item-add-${id}`}>Add</button>
+      <button className={`controls-button shop-item-add shop-item-add-${id}`}
+              data-item-id={id}
+              data-item-image-url={imageUrl}
+              data-item-name={name}
+              data-item-price={price}
+              onClick={addItemToCart}
+      >Add</button>
     </div>
   )
 }
 
 export default ShopItemView;
+
+/**
+ * Add shop item to the cart
+ * @param e mouse left button click event
+ */
+const addItemToCart = (e: React.MouseEvent<HTMLButtonElement>): void => {
+  const clickedButton = e.target as HTMLButtonElement;
+  const itemData: ItemData = {
+    count: 1,
+    id: parseInt(clickedButton.dataset.itemId as unknown as string, 10),
+    imageUrl: clickedButton.dataset.itemImageUrl as unknown as string,
+    name: clickedButton.dataset.itemName as unknown as string,
+    price: parseInt(clickedButton.dataset.itemPrice as unknown as string, 10),
+  };
+  
+  addShopItemToCart(itemData);
+}
