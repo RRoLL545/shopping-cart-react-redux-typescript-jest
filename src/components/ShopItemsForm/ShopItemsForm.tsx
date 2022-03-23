2e4945ad186a6cb5a74e863997bb335564b0c980
Быков Роман <bykov@softlab.ru>
2022-03-23 15:53:46 +0300

    feat(AddItemFromShop) AddItemFromShop refactor
    Refactor ShopItemsForm component with styles

--- src/components/ShopItemsForm/ShopItemsForm.tsx
+++ src/components/ShopItemsForm/ShopItemsForm.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useActions } from '../../hooks/useActions';
 import useTypedSelector from '../../hooks/useTypedSelector';
+import ShopItem from '../ShopItem/ShopItem';
 import './ShopItemsForm.css';
 
 /**
@@ -8,34 +9,39 @@ import './ShopItemsForm.css';
  * @returns ShopItemsForm component
  */
 const ShopItemsForm = (): JSX.Element => {
-  const {errorMessage, errorStatus, itemsLoading, shopItems} = useTypedSelector(state => state.shopState);
-  const {fetchShopItems} = useActions();
-  useEffect(() => {
-    fetchShopItems();
-  }, []);
+  // const {errorMessage, errorStatus, itemsLoading, shopItems} = useTypedSelector(state => state.shopState);
+  // const {fetchShopItems} = useActions();
+  // useEffect(() => {
+  //   fetchShopItems();
+  // }, []);
 
-  if (itemsLoading) {
-    return <h1>Loading...</h1>
-  }
-  if (errorStatus) {
-    return <h1>{errorMessage}</h1>
-  }
-  if (!shopItems.length) return ( // TODO change className
-    <div className="cart-items">
-      {'Strange. No items in the shop?'}
-    </div>
-  )
+  const [ searchTerm, setSearchTerm ] = useState('');
+  const getSearchTerm = (e: React.FormEvent<HTMLInputElement>) => {
+      setSearchTerm(e.currentTarget.value);
+    };
+  console.log(searchTerm)
+  // if (itemsLoading) {
+  //   return <h1>Loading...</h1>
+  // }
+  // if (errorStatus) {
+  //   return <h1>{errorMessage}</h1>
+  // }
+  // if (!shopItems.length) return ( // TODO change className
+  //   <div className="cart-items">
+  //     {'Strange. No items in the shop?'}
+  //   </div>
+  // )
 
   return (
     <>
       <div className="add-new-item"> {/* добавить invisible */}
         <h3 className="add-new-item-name">Add new item in to your cart</h3>
         <div className="search-input-wrapper">
-          {/*<input ref={inputEl} id="search" type="search" className="search-input" placeholder="Type item name" onChange={ getSearchTerm }></input>*/}
+          <input id="search" type="search" className="search-input" placeholder="Type item name" onChange={getSearchTerm} />
           <div className="search-icon"></div>
         </div>
         <div className="shop-items-add">
-          {/*<ShopItem term={searchTerm} />*/}
+          {/*<ShopItem  />*/}ShopItemsList
         </div>
       <hr className="items-horizontal-divider" />
       </div>
