58323002a7c800d7fd04466dc9f8478471ca316c
Быков Роман <bykov@softlab.ru>
2022-03-29 09:09:33 +0300

    feat(unit-testing): test Footer component
    Test Footer component
    Add extra test in Header component

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -7,4 +7,12 @@ describe('Header component testing', () => {
     const header = screen.getByRole('banner');
     expect(header).toBeEnabled();
   })
+
+  const navBarExtraClassName = 'navigation-bar-header';
+  it(`should render NavBar component with className="${navBarExtraClassName}" in Header`, () => {
+    render(<Header />);
+    const navBar = screen.getByRole('navigation');
+    expect(navBar).toBeEnabled();
+     expect(navBar.classList.contains(navBarExtraClassName)).toBeTruthy();
+  })
 })
