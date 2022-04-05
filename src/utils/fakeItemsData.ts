62f784f7cc8092e910ab62f2c3ec8db86bc684a8
Быков Роман <bykov@softlab.ru>
2022-04-05 08:21:56 +0300

    feat(unit-testing): test cartItemsReducer
    Test cartItemsReducer.
    Add fake data.

--- src/utils/fakeItemsData.ts
+++ src/utils/fakeItemsData.ts
@@ -1,7 +1,7 @@
 import { ItemData } from "../interfaces/itemData";
 
 /**
- * Fake items data for testing
+ * Fake items data for testing - default
  */
 const fakeItems: ItemData[] = [
   {
@@ -13,6 +13,122 @@ const fakeItems: ItemData[] = [
   },
   {
     count: 2,
+    id: 2,
+    imageUrl: 'any-image-url-2',
+    name: 'any-name-2',
+    price: 2
+  },
+  {
+    count: 3,
+    id: 3,
+    imageUrl: 'any-image-url-3',
+    name: 'any-name-3',
+    price: 3
+  },
+  {
+    count: 4,
+    id: 4,
+    imageUrl: 'any-image-url-4',
+    name: 'any-name-4-lol',
+    price: 4
+  },
+  {
+    count: 5,
+    id: 5,
+    imageUrl: 'any-image-url-5',
+    name: 'any-name-5-lol',
+    price: 5
+  }
+];
+
+/**
+ * Fake items data for testing - item with id 3 is removed
+ */
+export const fakeItemsReduced: ItemData[] = [
+  {
+    count: 1,
+    id: 1,
+    imageUrl: 'any-image-url-1',
+    name: 'any-name-1',
+    price: 1
+  },
+  {
+    count: 2,
+    id: 2,
+    imageUrl: 'any-image-url-2',
+    name: 'any-name-2',
+    price: 2
+  },
+  {
+    count: 4,
+    id: 4,
+    imageUrl: 'any-image-url-4',
+    name: 'any-name-4-lol',
+    price: 4
+  },
+  {
+    count: 5,
+    id: 5,
+    imageUrl: 'any-image-url-5',
+    name: 'any-name-5-lol',
+    price: 5
+  }
+];
+
+/**
+ * Fake items data for testing - count of item with id 2 is decreased
+ */
+ export const fakeItemsDecreased: ItemData[] = [
+  {
+    count: 1,
+    id: 1,
+    imageUrl: 'any-image-url-1',
+    name: 'any-name-1',
+    price: 1
+  },
+  {
+    count: 1,
+    id: 2,
+    imageUrl: 'any-image-url-2',
+    name: 'any-name-2',
+    price: 2
+  },
+  {
+    count: 3,
+    id: 3,
+    imageUrl: 'any-image-url-3',
+    name: 'any-name-3',
+    price: 3
+  },
+  {
+    count: 4,
+    id: 4,
+    imageUrl: 'any-image-url-4',
+    name: 'any-name-4-lol',
+    price: 4
+  },
+  {
+    count: 5,
+    id: 5,
+    imageUrl: 'any-image-url-5',
+    name: 'any-name-5-lol',
+    price: 5
+  }
+];
+
+/**
+ * Fake items data for testing - count of item with id 1 is increased
+ */
+ export const fakeItemsIncreased: ItemData[] = [
+  {
+    count: 2,
+    id: 1,
+    imageUrl: 'any-image-url-1',
+    name: 'any-name-1',
+    price: 1
+  },
+  {
+    count: 1,
     id: 2,
     imageUrl: 'any-image-url-2',
     name: 'any-name-2',
